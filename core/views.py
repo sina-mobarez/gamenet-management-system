21c4bf23cdc0cd7dc87f91e7b5636963b713f72d
Sina <mubarriizz@gmail.com>
2026-04-15 14:22:12 +0330

    fix : use python isort for clean codes

--- core/views.py
+++ core/views.py
@@ -1,17 +1,16 @@
-from datetime import timedelta
-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.http import JsonResponse
 from django.utils import timezone
-from django.db.models import Q, Count, Sum
+from django.db.models import Sum, Count, Q
 from decimal import Decimal, InvalidOperation
+from datetime import timedelta
 
 from .models import (
-    Device, Customer, ProductCategory, Session, SessionPlayer,
-    Product, Sale, Payment
+    Device, Customer, Session, SessionPlayer,
+    Product, ProductCategory, Sale, Payment
 )
 
 
